189fdaed6b3acdb2691ab6f860ea2c94c1dbc912
Aymeric TOULOUSE <atoulous@e1r11p17.42.fr>
2018-02-13 17:12:27 +0100

    tue13feb - atoulous - little refacto client, add react-router-dom

--- src/server/classes/Game.js
+++ src/server/classes/Game.js
@@ -3,18 +3,23 @@ class Game {
     this._room = room;
     this._players = [];
   }
+
   get room() {
     return this._room;
   }
+
   set room(room) {
     this._room = room;
   }
+
   get players() {
     return this._players;
   }
+
   set players(players) {
     this._players = players;
   }
+
   broadcast(io, subject, data) {
     this.players.forEach((player) => {
       io.to(player.socket).emit(subject, data);
