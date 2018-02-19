44be5a5b7ee726f1aba70171a0965a5c6ba0803a
Jordan MUNOZ <jmunoz@e2r12p9.42.fr>
2018-02-19 11:48:04 +0100

    Keep player actions.

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -2,10 +2,12 @@ import * as actionsConnexion from './connexion';
 import * as actionsTetris from './tetris';
 import * as actionsSocket from './socket';
 import * as actionsGame from './game';
+import * as actionPlayer from './player';
 
 export default {
   ...actionsTetris,
   ...actionsConnexion,
   ...actionsSocket,
-  ...actionsGame
+  ...actionPlayer,
+  ...actionsGame,
 };
