3882453a1ba2d71dd32970c4d7e66f39732003ce
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2018-02-23 16:41:43 +0100

    Web21feb atoulous (#34)
    * web21feb - atoulous
    
    * fri23feb - init bag back
    
    * fri23feb - add handlePieceSocket client-side

--- src/server/classes/Piece.js
+++ src/server/classes/Piece.js
@@ -9,7 +9,11 @@ import { getRandomPiece } from '../helpers/pieces';
  */
 class Piece {
   constructor(init) {
+<<<<<<< HEAD
     this.shape = get(init, 'shape', getRandomPiece());
+=======
+    this.form = get(init, 'form', getRandomPiece());
+>>>>>>> Web21feb atoulous (#34)
     this.direction = get(init, 'direction', 0);
     this.x = get(init, 'x', 0);
     this.y = get(init, 'y', 0);
