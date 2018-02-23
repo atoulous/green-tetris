4e61191da0ad0d82f0dc77be16c93a9815ac41f2
Aymeric TOULOUSE <atoulous@e2r13p7.42.fr>
2018-02-23 17:04:26 +0100

    fri23feb - hot fix merge

--- src/client/reducers/connexion.js
+++ src/client/reducers/connexion.js
@@ -1,11 +1,14 @@
-export function rtcConnexion(state, action) {
-  const conn = action.data;
-  const { RTCConns } = state;
-  RTCConns.push({ conn });
-  return { ...state, RTCConns };
-}
-
 export function rtcMessage(state) {
   console.log('successfully triggered action message --');
   return state;
+}
+
+export function addAudioStream(state, action) {
+  const { audioStreams } = state;
+  console.log('all streams are -- ', [...audioStreams, action.data]);
+  return { ...state, audioStreams: [...audioStreams, action.data] };
+}
+
+export function toggleMuted(state, action) {
+  return { ...state, muted: action.data.muted };
 }
