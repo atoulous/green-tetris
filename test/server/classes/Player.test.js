148c4829d6c3e7962879a27d6a073391fc57ce5d
Thibault FRANCOIS <thifranc42@gmail.com>
2018-03-01 12:00:13 +0100

    fix getPayload + add player Class tests

--- test/server/classes/Player.test.js
+++ test/server/classes/Player.test.js
@@ -1,36 +1,43 @@
 import Player from '../../../src/server/classes/Player';
+import Game from '../../../src/server/classes/Game';
+import MockSocket from './MockSocket';
 
 describe('classes/player', () => {
   let player;
 
-  beforeAll(() => {
-    player = new Player({ socketId: 'socketId', webRTCId: 'webRTCId', nickname: 'Player' });
-  });
-
-  it('should success to create player instance', async () => {
-    expect(player).not.toBeUndefined();
-    expect(player).toHaveProperty('nickname', 'Player');
-  });
-
   it('should fail to create player instance', async () => {
     function failPlayerCreation() {
-      return new Player({ no: true });
+      return new Player();
     }
     expect(failPlayerCreation).toThrowError(/required/);
   });
 
+  it('should success to create player instance', async () => {
+    player = new Player(new MockSocket());
+    expect(player).not.toBeUndefined();
+    expect(player.getPayload()).toHaveProperty('nickname', 'default');
+    expect(player.getPayload()).toHaveProperty('webRTCId', 'default');
+    expect(player.getPayload()).toHaveProperty('isReady', false);
+    expect(player.getPayload()).toHaveProperty('hasWon', null);
+    expect(player.getPayload()).toHaveProperty('score', 0);
+    expect(player.getPayload()).toHaveProperty('gameId', null);
+  });
+
   it('should change player nickname', async () => {
-    player.nickname = 'thibault';
-    expect(player).toHaveProperty('nickname', 'thibault');
+    player = new Player(new MockSocket());
+    player.set('nickname', 'thibault');
+    expect(player.getPayload()).toHaveProperty('nickname', 'thibault');
   });
 
-  it('should change player socketId', async () => {
-    player.socketId = 'thibault';
-    expect(player).toHaveProperty('socketId', 'thibault');
+  it('should update player', async () => {
+    player = new Player(new MockSocket());
+    player.update({ nickname: 'thibault' });
+    expect(player.getPayload()).toHaveProperty('nickname', 'thibault');
   });
 
-  it('should change player webRTCId', async () => {
-    player.webRTCId = 'thibault';
-    expect(player).toHaveProperty('webRTCId', 'thibault');
+  it('should set gameId of player on creation', async () => {
+    player = new Player(new MockSocket());
+    const game = new Game(player.get('id'));
+    expect(player.getPayload()).toHaveProperty('gameId', game.get('id'));
   });
 });
