2feceb4613d57219ca7aad7746050b8347b847b5
Thifranc <thifranc@student.42.fr>
2018-03-02 13:32:47 +0100

    add action tests

---
+++ test/client/actions/player.test.js
@@ -0,0 +1,46 @@
+import * as actions from '../../../src/client/actions/player.js';
+
+describe('OPEN_NICKNAME_MODAL', () => {
+    it('should create an action openNicknameModal', () => {
+          const expectedAction = {
+                  type: actions.OPEN_NICKNAME_MODAL,
+                }
+          expect(actions.openNicknameModal())
+            .toEqual(expectedAction)
+        })
+})
+
+describe('CLOSE_NICKNAME_MODAL', () => {
+    it('should create an action closeNicknameModal', () => {
+          const expectedAction = {
+                  type: actions.CLOSE_NICKNAME_MODAL,
+                }
+          expect(actions.closeNicknameModal())
+            .toEqual(expectedAction)
+        })
+})
+
+describe('UPDATE_NICKNAME', () => {
+    it('should create an action updateNickname', () => {
+          const nickname = 'thibault';
+          const expectedAction = {
+                  type: actions.UPDATE_NICKNAME,
+                  nickname
+                }
+          expect(actions.updateNickname(nickname))
+            .toEqual(expectedAction)
+        })
+})
+
+describe('UPDATE_PLAYER_ID', () => {
+    it('should create an action updatePlayerId', () => {
+          const id = 'oijoijoij';
+          const expectedAction = {
+                  type: actions.UPDATE_PLAYER_ID,
+                  id
+                }
+          expect(actions.updatePlayerId(id))
+            .toEqual(expectedAction)
+        })
+})
+
